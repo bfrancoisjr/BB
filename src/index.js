655a6f752035417af22ce4d7db538aef3e9817a6
Buteau Francois <bfrancois@kenzan.com>
2019-10-03 10:39:56 -0400

    revert devtools import

--- src/index.js
+++ src/index.js
@@ -12,9 +12,10 @@ import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order'
 import authReducer from './store/reducers/auth';
 
-const composeEnhancers = process.env.NODE_ENV === 'development'
-      ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-      : null;
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : compose;
 
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
